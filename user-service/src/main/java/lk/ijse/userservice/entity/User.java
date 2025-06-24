748187389c0e552bcc45bb065601b8bfc8d89754
sehansassara <sehansassara2002@gmail.com>
2025-06-25 03:05:36 +0530

    final makes

--- user-service/src/main/java/lk/ijse/userservice/entity/User.java
+++ user-service/src/main/java/lk/ijse/userservice/entity/User.java
@@ -13,7 +13,7 @@ public class User {
     private String name;
     private String email;
     private String password;
-    private String role; // e.g. "DRIVER" or "OWNER"
+    private String role;
 
     public User() {
     }
